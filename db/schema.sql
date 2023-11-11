6377385f45abe36de517d5456c9f7d8fd922b4b3
Nicole Slater <nicoleslater@pursuit.org>
2023-11-11 13:19:13 -0500

    Updating schema and seed

--- db/schema.sql
+++ db/schema.sql
@@ -7,18 +7,20 @@ CREATE DATABASE movies_dev;
 CREATE TABLE movies (
     id SERIAL PRIMARY KEY, 
     title TEXT NOT NULL, 
-    released DATE, 
+    url TEXT,
+    released DATE NOT NULL, 
     director TEXT,
     genre TEXT, 
-    rated TEXT, 
-    is_favorite BOOLEAN, 
-    imdb_Ratings INT
+    rated TEXT DEFAULT 'NR', 
+    is_favorite BOOLEAN DEFAULT FALSE, 
+    imdb_Ratings INTEGER DEFAULT 0
 );
 
--- CREATE TABLE favorites (
---     id SERIAL PRIMARY KEY, 
---     rating NUMERIC,
---     CHECK (rating >= 0 AND rating <= 5),
---     movie_id INTEGER REFERENCES movies(id) 
---     ON DELETE CASCADE
--- )
+CREATE TABLE favorites (
+    id SERIAL PRIMARY KEY, 
+    notes TEXT,
+    rating NUMERIC,
+    CHECK (rating >= 0 AND rating <= 5),
+    movie_id INTEGER REFERENCES movies(id) 
+    ON DELETE CASCADE
+)
--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,7 @@
 \c movies_dev;
 
-INSERT INTO movies (title, released, director, genre, rated, is_favorite, imdb_Ratings)
+INSERT INTO movies (title, url, released, director, genre, rated, is_favorite, imdb_Ratings)
 VALUES 
-('Rush Hour', '1998-09-18','Brett Ratner', 'Action', 'PG-13', true, 7),
-('Kill Bill', '2003-10-10', 'Quentin Tarantino', 'Action', 'R', true, 8.2), 
-('Sharknado', '2013-07-11', 'Anthony C. Ferrante', 'Action', 'Not Rated', false, 3.3);
+('Rush Hour', 'https://www.imdb.com/title/tt0120812', '1998-09-18','Brett Ratner', 'Action', 'PG-13', true, 7),
+('Kill Bill: Vol. 1', 'https://www.imdb.com/title/tt0266697', '2003-10-10', 'Quentin Tarantino', 'Action', 'R', true, 8.2), 
+('Sharknado','https://www.imdb.com/title/tt2724064', '2013-07-11', 'Anthony C. Ferrante', 'Action', 'Not Rated', false, 3.3);
