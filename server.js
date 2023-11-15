93b86dbba1f56f0599a66cc89a27cf566f28ca47
Nicole Slater <nicoleslater@pursuit.org>
2023-11-14 21:21:01 -0500

    Upating server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app.js");
 
-const PORT = process.env.PORT || 3434;
+const PORT = process.env.PORT || 7777;
 
 app.listen(PORT, () => {
     console.log(`Movies live on port: ${PORT}`)
