1085cf4b4cb97ed17b3d185a71b6dcd287aff692
Nicole Slater <nicoleslater@pursuit.org>
2023-11-16 19:40:12 -0500

    PSQL Schema and Seed

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const favorites = express.Router({mergeParams: true});
+const favorites = express.Router({ mergeParams: true });
 
 const { getOneMovie } = require("../queries/movies.js");
 
