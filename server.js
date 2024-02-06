361d809083de6849a6a8951a1a9f91f389622ffc
Nicole Slater <nicoleslater@pursuit.org>
2024-02-06 00:14:15 -0500

    Updating Backend Folders and Files

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app.js");
-
-require("dotenv").config();
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Movies live on port: ${PORT}`)
-});
-
