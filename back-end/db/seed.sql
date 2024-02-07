957b5cff4c13dcf90001c8b09b43db45b973db05
Nicole Slater <nicoleslater@pursuit.org>
2024-02-07 13:26:08 -0500

    Debugging in the seed sql

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -11,6 +11,6 @@ VALUES
 INSERT INTO favorites (first_name, title, notes, rating)
 VALUES 
 ('Kim', 'Rush Hour', 'I love Chris Tucker! Great Movie!', 5),
-('Tisha', 'Waiting to Exhale', 'I love watching Whitney on the screen!',5) 
+('Tisha', 'Waiting to Exhale', 'I love watching Whitney on the screen!', 5), 
 ('Caleb', 'Kill Bill: Volume 1', 'Ready for the Sequel!', 5), 
 ('John', 'Sharknado', 'Too cheesy!', 3);
